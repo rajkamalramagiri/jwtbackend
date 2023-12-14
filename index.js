969e5438aa08ef3c6afd4498d8d340a6115f4b9d
“Surendra” <v-surendra.raj@shutterfly.com>
2023-12-14 10:52:19 +0530

    JWT ADDED

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+
+const app = express();
+const PORT = 5000;
+
+// Replace this secret with a secure secret key for production
+const JWT_SECRET = 'your-secret-key';
+
+app.use(bodyParser.json());
+
+// Dummy user data (for demonstration purposes)
+const users = [
+  { id: 1, username: 'user1', password: 'password1' },
+  { id: 2, username: 'user2', password: 'password2' },
+];
+
+// Middleware to check if the request has a valid token
+const authenticateToken = (req, res, next) => {
+  const token = req.headers.authorization?.split(' ')[1];
+
+  if (!token) {
+    return res.sendStatus(401);
+  }
+
+  jwt.verify(token, JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.sendStatus(403);
+    }
+
+    req.user = user;
+    next();
+  });
+};
+app.get('/test', (req, res) => {
+  res.send('hello')
+})
+
+// Route to handle user login
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  const user = users.find(u => u.username === username && u.password === password);
+
+  if (!user) {
+    return res.status(401).json({ message: 'Invalid username or password' });
+  }
+
+  const token = jwt.sign({ id: user.id, username: user.username }, JWT_SECRET, { expiresIn: '1h' });
+
+  res.json({ token });
+});
+
+// Protected route that requires a valid token
+app.get('/protected', authenticateToken, (req, res) => {
+  res.json({ message: 'You have access to this protected route', user: req.user });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
